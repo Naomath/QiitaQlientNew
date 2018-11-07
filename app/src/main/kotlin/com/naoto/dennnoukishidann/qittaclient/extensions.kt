ebac6c04c5d54d54a709ca6a461fa55cad389340
naomath <math1972naonao@gmail.com>
2018-11-07 18:39:21 +0900

    revive build file

--- app/src/main/kotlin/com/naoto/dennnoukishidann/qittaclient/extensions.kt
+++ app/src/main/kotlin/com/naoto/dennnoukishidann/qittaclient/extensions.kt
@@ -14,3 +14,7 @@ fun <T : View> View.bindView(@IdRes id: Int): Lazy<T> = lazy { findViewById<View
 fun Context.toast(message: String, duration: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(this, message, duration).show()
 }
+
+
+
+
